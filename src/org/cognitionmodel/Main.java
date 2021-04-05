9a716cd60e4683afeaaabe5e7f80c22e2205e88f
YuriyMikheev <yuri.mikheev@gmail.com>
2021-04-05 14:28:56 +0300

    Hello!

---
+++ src/org/cognitionmodel/Main.java
@@ -0,0 +1,9 @@
+package org.cognitionmodel;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello from Artificial Cognitive System!");
+    }
+}
